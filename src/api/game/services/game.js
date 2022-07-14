172d5053a1972c261893215536a17f38b775d800
Gustavo Jezini <Gustavojezini@gmail.com>
2022-07-14 11:25:00 -0300

    feat: create game

--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -31,7 +31,6 @@ async function getByName(name, entityName) {
         name: { $eq: name}
       }
     });
-    console.log("Item: ", results);
   return results.length ? results[0] : null;
 }
 
@@ -49,6 +48,71 @@ async function create(name, entityName) {
   }
 }
 
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((item) => {
+        categories[item] = true;
+      });
+    supportedOperatingSystems &&
+      supportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.service('api::game.game').create(
+          { data: {
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        }
+      });
+
+        return game;
+      }
+    })
+  );
+}
+
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::game.game', ({ strapi }) => ({
@@ -60,8 +124,11 @@ module.exports = createCoreService('api::game.game', ({ strapi }) => ({
 
     // console.log(products[0]);
 
-    await create(products[1].publisher, "publisher");
-    await create(products[1].developer, "developer");
+    await createManyToManyData([products[2], products[3]])
+    await createGames([products[2], products[3]])
+
+    // await create(products[1].publisher, "publisher");
+    // await create(products[1].developer, "developer");
 
     // console.log(await getGameInfo(products[0].slug));
   },
